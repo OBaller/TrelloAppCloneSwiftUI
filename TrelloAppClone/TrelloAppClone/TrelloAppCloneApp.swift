a3f5789b8bc73f6e6d634caebc78102c6db8fe7a
brightsummit.co <naseemoyebola@gmail.com>
2021-12-06 17:18:27 +0100

    initial Commit

---
+++ TrelloAppClone/TrelloAppClone/TrelloAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrelloAppCloneApp.swift
+//  TrelloAppClone
+//
+//  Created by IBE on 02/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct TrelloAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
